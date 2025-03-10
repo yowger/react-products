96083e6165d89de5ad6c80bc3d24b3c4155f8158
Ver0018 <rogerpantilyowger@gmail.com>
2025-03-10 23:24:25 +0800

    tanstack query init, moved app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
 import ProductPage from "@/features/products/pages/ProductPage"
-import React from "react"
+import AppLayout from "@/layouts/AppLayout"
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 1000 * 60 * 5,
+            refetchOnWindowFocus: false,
+            retry: 2,
+        },
+    },
+})
 
 export default function App() {
     return (
-        <div>
+        <QueryClientProvider client={queryClient}>
             <AppLayout>
                 <ProductPage />
             </AppLayout>
-        </div>
-    )
-}
-
-interface AppLayoutProps {
-    children: React.ReactNode
-}
-
-function AppLayout(props: AppLayoutProps) {
-    const { children } = props
-
-    return (
-        <div className="p-4">
-            <div className="flex flex-col mx-auto max-w-7xl">{children}</div>
-        </div>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     )
 }
