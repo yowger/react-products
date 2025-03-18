59ceca86bb654302d7c3967d9551824a71207379
Ver0018 <rogerpantilyowger@gmail.com>
2025-03-19 01:48:13 +0800

    modify logout button

--- src/components/auth/logOutButton.tsx
+++ src/components/auth/logOutButton.tsx
@@ -1,8 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react"
 
 import { Button } from "@/components/ui/button"
+import React from "react"
 
-export default function LogoutButton() {
+interface LogoutButtonProps
+    extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "onClick"> {
+    children?: React.ReactNode
+}
+
+export default function LogoutButton(props: LogoutButtonProps) {
+    const { children, ...restProps } = props
+
     const { logout } = useAuth0()
 
     return (
@@ -10,8 +18,9 @@ export default function LogoutButton() {
             onClick={() =>
                 logout({ logoutParams: { returnTo: window.location.origin } })
             }
+            {...restProps}
         >
-            Log Out
+            {children || "Log out"}
         </Button>
     )
 }
