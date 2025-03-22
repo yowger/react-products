ae38149f2611915ec9c2cccff0937ecba8a28468
Ver0018 <rogerpantilyowger@gmail.com>
2025-03-22 14:28:37 +0800

    auth0 secuirity measures

--- src/providers/auth0Provider.tsx
+++ src/providers/auth0Provider.tsx
@@ -12,7 +12,10 @@ export default function Auth0Provider(props: Auth0ProviderProps) {
             clientId={config.auth.VITE_AUTH_CLIENT_ID}
             authorizationParams={{
                 redirect_uri: window.location.origin,
+                audience: config.auth.VITE_AUTH_AUDIENCE,
             }}
+            cacheLocation="memory"
+            useRefreshTokens={true}
         >
             {children}
         </Auth0LibProvider>
