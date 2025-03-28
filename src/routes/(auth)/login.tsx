7af253f89f0162a91b88d49b01bb15bcf0b3f887
Ver0018 <rogerpantilyowger@gmail.com>
2025-03-29 04:28:50 +0800

    added login redirect if login

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, redirect } from "@tanstack/react-router"
 
 import { Button } from "@/components/ui/button"
 import { useLogin } from "@/hooks/useLogin"
+import { z } from "zod"
+
+const fallback = "/posts" as const
 
 export const Route = createFileRoute("/(auth)/login")({
+    validateSearch: z.object({
+        redirect: z.string().optional().catch(""),
+    }),
+    beforeLoad: ({ context, search }) => {
+        if (context.isAuthenticated) {
+            throw redirect({
+                to: search.redirect || fallback,
+            })
+        }
+    },
     component: RouteComponent,
 })
 
