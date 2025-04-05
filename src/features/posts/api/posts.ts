e68bf3d299169422d5020f597ec86cca91e71018
Ver0018 <rogerpantilyowger@gmail.com>
2025-04-06 05:05:09 +0800

    comment unused (for now) code

--- src/features/posts/api/posts.ts
+++ src/features/posts/api/posts.ts
@@ -1,6 +1,6 @@
 import publicAxios from "@/api/axios/publicAxios"
 import type { GetPostsParams, Post } from "@/types/post"
-import { createQueryString } from "@/lib/utils/queryParams"
+// import { createQueryString } from "@/lib/utils/queryParams"
 import { camelizeKeys } from "@/lib/utils/camelizeKeys"
 
 const TABLE_POSTS = "posts"
@@ -11,13 +11,13 @@ export const POSTS_API = {
 }
 
 export async function getPosts(params?: GetPostsParams): Promise<Post[]> {
-    const queryString = createQueryString({
-        limit: params?.limit,
-        offset: params?.offset,
-        title: params?.search ? `ilike.%${params.search}%` : undefined,
-        order: params?.orderBy,
-        orderDirection: params?.orderDirection || "desc",
-    })
+    // const queryString = createQueryString({
+    //     limit: params?.limit,
+    //     offset: params?.offset,
+    //     title: params?.search ? `ilike.%${params.search}%` : undefined,
+    //     order: params?.orderBy,
+    //     orderDirection: params?.orderDirection || "desc",
+    // })
 
     const url = `${POSTS_API.GET_POSTS}?select=*,author_id(*)`
     //  const url = `${POSTS_API.GET_POSTS}${queryString ? `?${queryString}` : ""}`
